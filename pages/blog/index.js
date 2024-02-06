c4637a2d641290812c0aee0ed48b2b4bcf8609d2
itc-n23004 <n23004@std.it-colloege>
2024-02-06 09:04:55 +0000

    9.6 finished
    記事一覧にアイキャッチ画像を追加

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,7 +3,10 @@ import Container from 'components/container'
 import Hero from 'components/hero'
 import Meta from 'components/meta'
 import Posts from 'components/posts'
+import { getPlaiceholder } from 'plaiceholder'
 
+// ローカル代替アイキャッチ画像
+import { eyecatchLocal } from 'lib/constants'
 const Blog = ({ posts }) => {
   return (
     <Container>
@@ -17,6 +20,15 @@ const Blog = ({ posts }) => {
 
 const getStaticProps = async () => {
   const posts = await getAllPosts()
+
+  for (const post of posts) {
+    if (!Object.prototype.hasOwnProperty.call(post, 'eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
 
   return {
     props: {
